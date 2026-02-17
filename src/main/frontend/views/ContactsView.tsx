1ea84bf5b2c19c6511278b27ec40c678a010594a
Nasrin <n.mokhtari@>
2026-02-17 12:14:18 +0330

    Define Listener

--- src/main/frontend/views/ContactsView.tsx
+++ src/main/frontend/views/ContactsView.tsx
@@ -2,12 +2,24 @@ import { ViewConfig } from '@vaadin/hilla-file-router/types.js';
 import {AutoGrid} from "@vaadin/hilla-react-crud";
 import {ContactService} from "Frontend/generated/endpoints";
 import ContactModel from "Frontend/generated/com/example/application/data/ContactModel";
+import {useState} from "react";
+import Contact from "Frontend/generated/com/example/application/data/Contact";
 export const config: ViewConfig = { menu: { order: 2, icon: 'line-awesome/svg/user.svg'}, title: 'Contact' };
 
 export default function ContactsView() {
+    const [selected, setSelected] = useState<Contact | null>();
+
     return (
         <div className="p-m">
-            <AutoGrid service={ContactService} model={ContactModel}  />
+            <AutoGrid
+                service={ContactService}
+                model={ContactModel}
+                onActiveItemChanged={e => setSelected(e.detail.value)}
+                selectedItems={[selected]}
+            />
+
+            {selected?.firstName}
+
         </div>
     );
 }
